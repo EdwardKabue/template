2a115caee43fff1cf53888867d30b4975a13f06e
Edward <kabueedward@yahoo.com>
2014-02-14 17:44:34 +0300

    my first code commitments

---
+++ js/main.js
@@ -0,0 +1,10 @@
+alert("Yay javascript!");
+console.log("I'm in the console");
+
+function myFunct(){
+  alert("You clicked!");
+}
+
+var myVar = document.getElementsByTagName('h1');
+console.log(myVar);
+
---
+++ js/myJquery.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('h1').on('click',function(){
+    alert("you clicked on h1!");
+  });
+
+});
